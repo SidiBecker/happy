34696b162b9019917d3844e09020b2d403dee6f4
Sidi Becker <sidibecker@hotmail.com>
2020-10-14 16:45:25 -0300

    Validação de erros.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import errorHandler from './errors/handler';
 
 import './database/connection';
+import 'express-async-errors';
 
 import routes from './routes';
 
@@ -9,5 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errorHandler);
 
 app.listen(3333);
